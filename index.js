1ea9412623992db22fb75d8e3ebc142b93f91866
AcauRangel <acauarangel17@gmail.com>
2024-01-27 22:07:13 -0300

    Add rotes

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/html/index.html");
+});
+
+app.get('/curriculo', function(req, res) {
+    res.sendFile(__dirname + "/html/curriculum.html");
+});
+
+app.get('/contato', function(req, res) {
+    res.sendFile(__dirname + "/html/contact.html");
+});
+
+app.listen(3000, () => 
+console.log('Servidor iniciado na porta 3000')
+);
